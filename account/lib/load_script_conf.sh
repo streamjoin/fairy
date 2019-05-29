b23097797682cb9978aa9a357361fd6b9a2ff18f
Qian Lin <linqian.qian.lin@gmail.com>
2019-05-29 14:52:31 +0800

    Fixed: specifying file list in array

--- account/lib/load_script_conf.sh
+++ account/lib/load_script_conf.sh
@@ -10,11 +10,10 @@ DEFAULT_ACCOUNT_SCRIPT_CONF="${HOME}/account/conf/account_script.conf.default"
 readonly SCRIPT_CONF="${ACCOUNT_SCRIPT_CONF:-${DEFAULT_ACCOUNT_SCRIPT_CONF}}"
 [[ -n "${SCRIPT_CONF}" ]] ||
 check_err "Script configuration is not specified"
+
 [[ -f "${SCRIPT_CONF}" ]] ||
 check_err "Invalid path of script configuration: ${SCRIPT_CONF}"
 
+# shellcheck disable=SC1090
 source "${SCRIPT_CONF}" ||
 check_err "Failed to load script configuration: ${SCRIPT_CONF}"
-
-# Identify script itself
-readonly SCRIPT_NAME="$(basename -- "$0")"
